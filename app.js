8f1f9f4d7e59930d62d7464ec976f80ba6b262e2
Damien White <damienkwhite@gmail.com>
2023-11-20 20:55:15 +0000

    completed js and tweaked a few styling elements for desktop view

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const input = document.querySelector("input");
+const errMsg = document.querySelector(".emailErrMsg")
 
 input.addEventListener("input", (event) => {
     event.preventDefault()
     if( input.value.includes("@") && !input.value.includes(" ")){
         console.log("it's valid")
+        input.style.border = "2px solid hsl(223, 100%, 88%)";
+        errMsg.style.visibility = "hidden";
     } else {
         console.log("it's not valid")
+        input.style.border = "2px solid red";
+        errMsg.style.visibility = "visible";
     }
 });
